c576c20143f357970246371523f0ac1ac206585d
Roger Causto <the.causto@gmail.com>
2015-04-27 18:42:10 +0200

    daily changes

--- source/js/main.js
+++ source/js/main.js
@@ -14,34 +14,44 @@ function pasteString(e) {
 function enterKey() {
 
     var _inputcontent = cardInput.value,
-    _notANumber = isNaN( _inputcontent );
+        _firstnumber = _inputcontent.charAt( 0 ),
+        _secondNumber = _inputcontent.charAt( 1 ),
+        _notANumber = isNaN( _inputcontent );
 
-    if ( _notANumber ){
+    if ( _notANumber ) {
 
         cardInput.value = _inputcontent.substring( 0, _inputcontent.length - 1 );
 
+    } else if ( _firstnumber != 3 && _firstnumber != 4 && _firstnumber != 5) {
+
+        console.log('not a valid start');
+
     } else {
 
-        var _visa,
-            _mastercard,
-            _americanExp;
+        var _buttonName = el('.input-group-addon'),
+            _visa = 'visa',
+            _mastercard = 'Master Card',
+            _americanExp = 'American Express';
 
         if ( _inputcontent.charAt( 0 ) == 4 && _inputcontent.length == 13 ) {
-            
-            /* check Bigger */
-            if ( _inputcontent.length > 13){
-                console.log('tooBigMotherFucker');
-            }
 
-            console.log('visa');
+            _buttonName.classList.add('valid');
+            _buttonName.innerHTML = _visa;
 
         } else if ( _inputcontent.charAt( 0 ) == 5 && _inputcontent.length == 16) {
 
-            console.log('masterCard');
+            _buttonName.classList.add('valid');
+            _buttonName.innerHTML = _mastercard;
+
+        } else if ( _inputcontent.charAt( 0 ) == 3 && ( _secondNumber == 4 || _secondNumber == 7 ) && _inputcontent.length == 15) {
+
+            _buttonName.classList.add('valid');
+            _buttonName.innerHTML = _americanExp;
 
         } else {
 
-            return;
+            _buttonName.classList.remove('valid');
+            _buttonName.innerHTML = '';
 
         }
 
@@ -49,5 +59,25 @@ function enterKey() {
 
 }
 
+function isEmpty(e) {
+
+    e.preventDefault();
+
+    var inputs = document.querySelectorAll('.form-control');
+
+    for (var i = 0; i < inputs.length; i++) {
+
+        if ( inputs[i].value == '' || null ) {
+            console.log('un champ est vide');
+        }
+        inputs[i];
+        
+    }
+
+
+}
+
 cardInput.addEventListener('paste', pasteString, false);
 cardInput.addEventListener('keyup', enterKey, false);
+
+el('.btn').addEventListener('click', isEmpty, false);
